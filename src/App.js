12d214d1d51d5b0e7b44a79f8fba58d9a9522622
Luis Cruz <cruz.torrez@gmail.com>
2021-02-20 15:01:01 -0500

    add redirection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import customProtocolCheck from "custom-protocol-check";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    customProtocolCheck("gaindemo://", () => {
+      window.location = "https://demo.gainapp.com/";
+    });
+  }, []);
+
+  return <div className="App">Redirecting</div>;
 }
 
 export default App;
